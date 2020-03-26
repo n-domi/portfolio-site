e336d3c76b9da4055db0c3d987e332bb366f2bef
n-domi <59164751+n-domi@users.noreply.github.com>
2020-03-26 11:10:16 +0900

    hoge

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,11 @@
+$(function(){
+  // #で始まるアンカーをクリックした場合に処理
+  $('a[href^="#"]').click(function() {
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+ });
+});
